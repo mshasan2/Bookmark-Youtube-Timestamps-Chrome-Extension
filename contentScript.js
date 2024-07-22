e362f5f4d5ec09496bc941607398f487e03ec415
mshasan2 <55490970+mshasan2@users.noreply.github.com>
2024-07-21 20:43:40 -0400

    Adding Button to player

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,35 @@
 (() => {
     let youtubeLeftControls, youtubePlayer;
+    let currentVideo = "";
+
+    chrome.runtime.onMessage.addListener((obj, sender, response)=>{
+        // Get the video ID from the message
+        const {type, value, videoId } = obj;
+        // Check if the message is of type "NEW"
+        if (type === "NEW") {
+            currentVideo = videoId;
+            newVideoLoaded();
+        }
+    });
+
+    const newVideoLoaded = () => {
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+        console.log(bookmarkBtnExists);
+
+        if (!bookmarkBtnExists) {
+            const bookmarkBtn = document.createElement("img");
+
+            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+            bookmarkBtn.title = "Click to bookmark current timestamp";
+
+            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+            
+            youtubeLeftControls.append(bookmarkBtn);
+            //bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+        }
+    }
+
+    newVideoLoaded();
 })();
